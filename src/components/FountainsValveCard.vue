0914d91848d026ed2348ab24a631b3a45d6cd7d9
templier1994 <jerome.amos@hevs.ch>
2021-06-07 14:32:13 +0200

    add fountainsValveCard component to reuse it into main page

---
+++ src/components/FountainsValveCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+      <b-card>
+          
+          <h4 class="card.title">Etat de la vanne {{this.$route.name}}
+            <i v-bind:class="[hovered ? 'fa fa-info-circle text-muted animate__animated animate__rubberBand' : 'fa fa-info-circle text-muted']"
+          v-on:mouseover="hovered=true" v-on:mouseout="hovered=false" @click="info = !info">
+          </i>
+        </h4>
+          
+          <h5>Horaire de fonctionnement : {{startTime}}-{{stopTime}} </h5>
+          <h5 v-if="valveState===0">Etat actuel : fermée  <img src="../assets/sensorNotOk.png" align="center"> </h5>
+          <h5 v-if="valveState===1">Etat actuel : ouverte  <img src="../assets/sensorOk.png" align="center"> </h5>
+          <h5 v-if="valveState===2">Etat actuel : en transition  <img src="../assets/sensorTransition.png" align="center"> </h5>
+          
+          <div class="container">
+          <div class="row justify-content-md-center" v-if="valveState === 0">
+            <button type="button" class="btn btn-success" v-on:click="toggleValve">Ouvrir</button>
+          </div>
+          
+          <div class="row justify-content-md-center" v-if="valveState === 1">
+            <button type="button" class="btn btn-danger" v-on:click="toggleValve">Fermer</button>
+          </div>
+
+          <div class="row justify-content-md-center" v-if="valveState === 2">
+            <button type="button" class="btn btn-light"  disabled = "disabled">En transition</button>
+          </div>
+
+          </div>
+           <!--Additionnal informations-->
+          <div v-if="info" class="animate__animated animate__fadeInDown" transition="zoomInOut">
+            <br>
+            <h5>Le système de communication que nous utilisons ne permet pas d'envoyer des ordres d'ouvertures ou de fermeture en tout temps. <br> L'état "en transition"
+            survient durant le temps d'attente de réception de la vanne. En principe, ce temps est de 10 minutes.</h5>
+          </div> 
+        </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FountainsValveCard",
+    props: {
+      location: String,
+      startTime : String,
+      stopTime : String,
+    }
+    ,
+    data() {
+      return {
+        valveState :0,
+        hovered: false,
+        info:false
+      }
+    },
+    mounted() {
+      //console.log("Date", this.timeStamp)
+    },
+    methods:{
+         /**
+       * Function that toggle the valve and send a message to the sensor
+       */
+      toggleValve : function (){
+        if(this.valveState === 1){
+          console.log("close Valve")
+          this.valveState = 2
+        }else{
+          console.log("open valve")
+          this.valveState = 2
+        }
+      },
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- src/pages/Dashboard/Fountains.vue
+++ src/pages/Dashboard/Fountains.vue
@@ -2,40 +2,11 @@
   <div> 
     <div class="row justify-content-md-center">
       <div class="col-lg-4">
-        <b-card>
-          
-          <h4 class="card.title">Etat de la vanne 
-            <i v-bind:class="[hovered ? 'fa fa-info-circle text-muted animate__animated animate__rubberBand' : 'fa fa-info-circle text-muted']"
-          v-on:mouseover="hovered=true" v-on:mouseout="hovered=false" @click="info = !info">
-          </i>
-        </h4>
-          
-          <h5>Horaire de fonctionnement : {{startTime}}-{{stopTime}} </h5>
-          <h5 v-if="valveState===0">Etat actuel : fermée  <img src="../../assets/sensorNotOk.png" align="center"> </h5>
-          <h5 v-if="valveState===1">Etat actuel : ouverte  <img src="../../assets/sensorOk.png" align="center"> </h5>
-          <h5 v-if="valveState===2">Etat actuel : en transition  <img src="../../assets/sensorTransition.png" align="center"> </h5>
-          
-          <div class="container">
-          <div class="row justify-content-md-center" v-if="valveState === 0">
-            <button type="button" class="btn btn-success" v-on:click="toggleValve">Ouvrir</button>
-          </div>
-          
-          <div class="row justify-content-md-center" v-if="valveState === 1">
-            <button type="button" class="btn btn-danger" v-on:click="toggleValve">Fermer</button>
-          </div>
-
-          <div class="row justify-content-md-center" v-if="valveState === 2">
-            <button type="button" class="btn btn-light"  disabled = "disabled">En transition</button>
-          </div>
-
-          </div>
-           <!--Additionnal informations-->
-          <div v-if="info" class="animate__animated animate__fadeInDown" transition="zoomInOut">
-            <br>
-            <h5>Le système de communication que nous utilisons ne permet pas d'envoyer des ordres d'ouvertures ou de fermeture en tout temps. <br> L'état "en transition"
-            survient durant le temps d'attente de réception de la vanne. En principe, ce temps est de 10 minutes.</h5>
-          </div> 
-        </b-card>
+            <fountains-valve-card 
+              :location="this.$route.name" 
+              :startTime="this.startTime.toString()" 
+              :stopTime="this.stopTime.toString()" >
+            </fountains-valve-card>
       </div>
     </div>
 
@@ -98,13 +69,14 @@
 
 <script>
 
+import FountainsValveCard from '../../components/FountainsValveCard.vue'
+
   export default {
-    components: {},
+    components: {FountainsValveCard},
     name: "Fountains",
     
-    component: {
-      
-    },
+    
+        
     data() {
       return {
         locationName: this.$route.name,             //route of the page
@@ -113,8 +85,7 @@
         stopTime : 0,
         newStartTime : "",
         newStopTime : "",
-        hovered: false,
-        info:false
+
 
       }
     },
@@ -147,18 +118,7 @@
 
 
     methods: {
-      /**
-       * Function that toggle the valve and send a message to the sensor
-       */
-      toggleValve : function (){
-        if(this.valveState === 1){
-          console.log("close Valve")
-          this.valveState = 2
-        }else{
-          console.log("open valve")
-          this.valveState = 2
-        }
-      },
+
 
       /**
        * validate the time activation change
