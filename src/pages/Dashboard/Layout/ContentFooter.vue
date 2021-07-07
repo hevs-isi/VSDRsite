6251160c2c79002d44dd9d34e496342ae0dc4c98
templier1994 <jerome.amos@hevs.ch>
2021-07-07 16:12:16 +0200

    start gateway check into technical page

--- src/pages/Dashboard/Layout/ContentFooter.vue
+++ src/pages/Dashboard/Layout/ContentFooter.vue
@@ -24,7 +24,7 @@ export default {
       // Do not rename the following, it is updated
       // automatically by the build script located
       // in packages.json
-      version: '4b96925a-07.07.21'
+      version: 'ccbfc4b0-07.07.21'
     }
   },
 }
--- src/pages/Dashboard/Technical.vue
+++ src/pages/Dashboard/Technical.vue
@@ -5,5 +5,103 @@
 </template>
 
 <script>
-  
+
+import axios from 'axios'
+import chirpstackCredentials from '../../constants/chirpstack.json'      //credentials for chirpstack server
+
+
+   export default {
+  components: {  },
+    name: "Technical",
+    
+    
+        
+    data() {
+      return {
+        gateway : [],
+        sensor : [],
+
+
+
+
+      }
+    },
+    mounted() {
+      for(let i = 0; i< this.$SENSORSLISTJSON.length; i++){
+        if(this.$SENSORSLISTJSON[i].project.toLowerCase() === this.$PROJECT){
+          if(this.$SENSORSLISTJSON[i].type === 'gateway'){
+            var gw = {
+                position_name: this.$SENSORSLISTJSON[i].location,
+                position: this.$SENSORSLISTJSON[i].coordinates,
+                eui: this.$SENSORSLISTJSON[i].eui,
+                lastSeen: "-",
+                timeStamp: "",
+                isUp: false,
+            }
+            this.gateway.push(gw)
+
+          }else if(this.$SENSORSLISTJSON[i].type === 'Fontaine' || this.$SENSORSLISTJSON[i].type === "Hauteur d'eau"){
+            var sen = {
+              position_name: this.$SENSORSLISTJSON[i].location,
+              position: this.$SENSORSLISTJSON[i].coordinates, 
+              isUp : null,
+              rssi : null,
+              snr : null
+            }
+            this.sensor.push(sen)
+          }
+        }
+      }
+
+       
+      var promises = []
+
+      for(let i = 0; i<this.gateway.length; i++){
+        promises.push(
+          axios(chirpstackCredentials.url+ 'gateways' + '/' + this.gateway[i].eui, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'origin', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'Grpc-Metadata-Authorization' : 'Bearer ' + chirpstackCredentials.token,
+            }
+          }))
+      }
+
+       Promise.all(promises)
+        .then((res) => {
+          for (let i = 0; i < res.length; i++) {
+            console.log(res[i].data)
+            //TODO : get gw info and check if online or not
+          }
+        })
+        .finally(x => {
+          setTimeout(() => {
+            this.pending = false;
+          }, 500
+        )
+        })
+      
+
+    },
+
+
+
+    methods: {
+
+      
+     
+    }
+
+
+   }
+
 </script>
+
+<style>
+  .lightText {
+    color: darkgrey;
+  }
+</style>
