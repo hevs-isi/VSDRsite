e0cf493700fd411e1b3200275d23ea886d18d0c2
templier1994 <jerome.amos@hevs.ch>
2021-08-04 10:49:08 +0200

    add to water height page discimination of valid value or not

--- src/pages/Dashboard/Layout/ContentFooter.vue
+++ src/pages/Dashboard/Layout/ContentFooter.vue
@@ -24,7 +24,7 @@ export default {
       // Do not rename the following, it is updated
       // automatically by the build script located
       // in packages.json
-      version: '727838c3-03.08.21'
+      version: 'cf5bd0f5-04.08.21'
     }
   },
 }
